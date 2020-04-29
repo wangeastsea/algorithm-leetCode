836288353b2b0b0bd95debafda715c182a80f119
wangeastsea <wangeastsea@icloud.com>
2020-04-29 10:03:54 +0800

    strStr()

---
+++ string/实现 strStr()/code.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+
+// 输入: haystack = "hello", needle = "ll"
+// 输出: 22
+// 1 直接使用api 有种胜之不武的赶脚
+var strStr = function (haystack, needle) {
+    return haystack.indexOf(needle)
+}
+
+// 2
+// 思路： 通过replace方法，将haystack中的needle全部替换成一个特殊字符$,然后循环计算$出现的位置即可，如果找不到就返回-1
+// var strStr = function (haystack, needle) {
+//     if (!needle) return 0
+//     const reg = new RegExp(`${needle}`, 'g')
+//     const a = haystack.replace(reg, '$')
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i] === '$') {
+//             return i
+//         }
+//     }
+//     return -1
+// }
+
+console.log(strStr('hello', 'll'))
+console.log(strStr('a', 'a'))
+console.log(strStr('aaa', 'aa'))
+console.log(strStr('aaa', ''))
