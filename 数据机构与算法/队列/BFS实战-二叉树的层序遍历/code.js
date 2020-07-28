a797d89dd50b8df14e7ce5d6d2c07fb8f4822eca
wangeastsea <wangeastsea@icloud.com>
2020-07-28 10:07:25 +0800

    二叉树层序遍历

---
+++ 数据机构与算法/队列/BFS实战-二叉树的层序遍历/code.js
@@ -0,0 +1,41 @@
+const root = {
+    val: 'A',
+    left: {
+        val: 'B',
+        left: {
+            val: 'D',
+        },
+        right: {
+            val: 'E',
+        },
+    },
+    right: {
+        val: 'C',
+        right: {
+            val: 'F',
+        },
+    },
+}
+
+function BFS(root) {
+    // 初始化队列queue
+    const queue = []
+    queue.push(root)
+    // 队列不为空，说明没有遍历完全
+    while (queue.length) {
+        // 取出队头元素
+        const top = queue[0]
+        // 访问 top
+        console.log(top.val)
+        // 如果左子树存在，左子树入队
+        if (top.left) {
+            queue.push(top.left)
+        }
+        // 如果左子树存在，右子树入队
+        if (top.right) {
+            queue.push(top.right)
+        }
+        // 访问完毕，队头元素出队
+        queue.shift()
+    }
+}
---
+++ 数据机构与算法/队列/BFS实战-二叉树的层序遍历/read.md
@@ -0,0 +1,18 @@
+> https://juejin.im/book/5cb42609f265da035f6fcb65/section/5cf3e215e51d457755550872
+
+> 现在我们要做的是对这个二叉树进行层序遍历。层序遍历的概念很好理解：按照层次的顺序，从上到下，从左到右地遍历一个二叉树，如图所示（红色数字即为遍历的序号）
+
+
+![image.png](https://upload-images.jianshu.io/upload_images/5016475-111c28ad4a5241fa.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240)
+
+
+正确的遍历序列为：
+
+```
+A
+B
+C
+D
+E
+F
+```
