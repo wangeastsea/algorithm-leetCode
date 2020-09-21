ab2473aec40578236d2f04b3d0930002cb1cf050
wangeastsea <wangeastsea@icloud.com>
2020-09-21 19:03:05 +0800

    update

---
+++ Array/485.最大连续1的个数/code.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var findMaxConsecutiveOnes = function (nums) {
+//     const len = nums.length
+//     const max1 = 0
+//     for (let i = 0; i < len; i++) {
+//         const array = []
+//         if (i === 1) {
+//             array.push[1]
+//         }
+//     }
+// }
