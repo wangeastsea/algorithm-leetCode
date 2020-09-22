5c488ff38eba472ce0c96561069ec2d0f7165a87
wangeastsea <wangeastsea@icloud.com>
2020-09-22 15:08:37 +0800

    arrat

--- Array/485.最大连续1的个数/code.js
+++ Array/485.最大连续1的个数/code.js
@@ -2,13 +2,75 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var findMaxConsecutiveOnes = function (nums) {
-//     const len = nums.length
-//     const max1 = 0
-//     for (let i = 0; i < len; i++) {
-//         const array = []
-//         if (i === 1) {
-//             array.push[1]
-//         }
-//     }
-// }
+
+// 时间复杂度是O(n)
+var findMaxConsecutiveOnes1 = function (nums) {
+    const len = nums.length
+    let max = 0
+    let array = []
+    for (let i = 0; i < len; i++) {
+        if (nums[i] > 0) {
+            array.push(1)
+        } else if (nums[i] === 0) {
+            // 为0
+            if (array.length > max) {
+                max = array.length
+            }
+            array = []
+        }
+    }
+    // 如果结尾是1，则在这里处理
+    if (array.length > 0) {
+        if (array.length > max) {
+            max = array.length
+        }
+        array = []
+    }
+    return max
+}
+var findMaxConsecutiveOnes2 = function (nums) {
+    const filterNums = nums
+        .toString()
+        .replace(/,/g, '')
+        .split(0)
+        .map((item) => {
+            return item.length
+        })
+
+    nums = filterNums.sort((a, b) => {
+        return a - b
+    })
+    return nums[nums.length - 1]
+}
+// leetcode 一个解答，跟我以上使用的方法很像，但是比我的简洁，这里拿来参考下
+var findMaxConsecutiveOnes3 = function (nums) {
+    const nString = nums.join('')
+    const nArry = nString.split(0)
+    // TODO 抽空研究下 sort
+    const lastPop = nArry.sort().pop()
+    const ln = lastPop.length
+    return ln
+}
+
+console.log(
+    findMaxConsecutiveOnes3([
+        1,
+        1,
+        0,
+        1,
+        1,
+        1,
+        0,
+        0,
+        1,
+        1,
+        0,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        0,
+    ])
+)
