87ef08ff060fa510eb6dcba24b17cc18a85c35f6
wangeastsea <wangeastsea@icloud.com>
2020-10-13 10:29:32 +0800

    array

---
+++ Array/41.缺失的第一个正数/code.js
@@ -0,0 +1,29 @@
+/** 获取引用指数
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function (citations) {
+    // 首先进行排序
+    citations.sort((a, b) => a - b)
+    const length = citations.length
+    if (!length) return 0
+    if (length === 1 && citations[0] !== 0) {
+        return 1
+    }
+    if (length === 1 && citations[0] === 0) {
+        return 0
+    }
+    let h
+    for (let i = 0; i < length; i++) {
+        // 使得当前值的大小（被引用次数) 等于包含当前值的后续数组的长度。
+        if (citations[i] === length - i) {
+            h = citations[i]
+            break
+        } else {
+            h = 0
+        }
+    }
+    return h
+}
+
+console.log(hIndex([0, 0]))
